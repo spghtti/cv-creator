05bb7091a8bc63d938980895051e44933aafe6f1
Matt Daniele <2266075+spghtti@users.noreply.github.com>
2022-03-25 19:44:52 -0400

    Updates

---
+++ precache-manifest.a1d4fe1e6b3219df5604e2450b38a171.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b4f7b0784d55e8f397c",
+    "url": "/cv-project/static/css/main.4a2a138c.chunk.css"
+  },
+  {
+    "revision": "7b4f7b0784d55e8f397c",
+    "url": "/cv-project/static/js/main.7b4f7b07.chunk.js"
+  },
+  {
+    "revision": "593487c6fc7917d82c3b",
+    "url": "/cv-project/static/js/1.593487c6.chunk.js"
+  },
+  {
+    "revision": "191a181b6a79c31ed82c",
+    "url": "/cv-project/static/js/runtime~main.191a181b.js"
+  },
+  {
+    "revision": "73ca8f1a36fad4ec001d64ce95bbad1d",
+    "url": "/cv-project/static/media/pin.73ca8f1a.png"
+  },
+  {
+    "revision": "a2adff50eb4de0f93d980d2c0a60ce83",
+    "url": "/cv-project/static/media/phone.a2adff50.png"
+  },
+  {
+    "revision": "42106b5ab253c1e4619303e1520a409d",
+    "url": "/cv-project/static/media/envelope.42106b5a.png"
+  },
+  {
+    "revision": "a349bd3c85c01f840626d467adcb23ab",
+    "url": "/cv-project/static/media/cursor.a349bd3c.png"
+  },
+  {
+    "revision": "8d6e9250e029b5ddfe5547cd6e506adc",
+    "url": "/cv-project/index.html"
+  }
+];
